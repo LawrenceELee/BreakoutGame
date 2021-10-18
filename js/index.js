0355faa718c7438a442f1ef6acd904850477d905
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-10-17 17:49:43 -0700

    Implemented code to draw paddle. Added comments to css.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 
+/* find the canvas element and start manipulating it */
+let canvas = document.getElementById("myCanvas");
+
+/* get the 2D rendering context */
+let context = canvas.getContext("2d");
+
+/* start manipulating the context object */
+context.beginPath();
+/* draw the paddle to hit ball */
+context.rect(20, 40, 50, 50);
+/* color the rect */
+context.fillStyle = "#FF0000";
+context.fill();
+/* finish manipulating context object */
+context.closePath();
