4bbf9bda57e3a311199227611833127e4e5c1854
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-10-17 19:31:10 -0700

    Began collision detection. Cleaned up CSS and HTML.

--- js/index.js
+++ js/index.js
@@ -13,11 +13,13 @@ let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
 
+let ballRadius = 10;
+
 function drawBall() {
 	context.beginPath();
 
 	/* draw ball */
-	context.arc(x, y, 10, 0, Math.PI*2);
+	context.arc(x, y, ballRadius, 0, Math.PI*2);
     context.fillStyle = "#0095DD";
     context.fill();
     context.closePath();
@@ -28,6 +30,14 @@ function draw() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	
 	drawBall();
+	
+	/* collison detection between ball and walls */
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
 	
 	/* the next coordinates to draw the new ball */
 	x += dx;
